261d73d69d9ba073778648aa8f07dc0396ef9fa7
John Doe <johndoe@example.com>
2024-06-30 23:45:52 +0200

    Ole

--- RGP/src/app/app.routes.ts
+++ RGP/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'app-game-logic',                                                    // Weiterleitung zur Startseite, wenn keine spezifische Route angegeben ist
+        pathMatch: 'full'
+    },
+    {
+        path: 'app-root',                                                                // Routenkonfiguration für die Root-Komponente der Anwendung
+        loadComponent: () => import('./app.component').then(m => m.AppComponent),
+    },
+    {
+        path: 'app-game-logic',                                                          // Routenkonfiguration für die Startseite-Komponente      
+        loadComponent: () => import('../game-logic/game-logic.component').then(m => m.GameLogicComponent),
+    },
+    
+];
