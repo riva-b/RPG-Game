a3ae438c340735db021e8733d0ac775ab48e2d7b
John Doe <johndoe@example.com>
2024-07-20 00:48:34 +0200

    ole

---
+++ Vorlagen/Tiled Map/treasure chests.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="treasure chests" tilewidth="16" tileheight="16" tilecount="120" columns="10">
+ <image source="treasure chests.png" trans="ffdcfa" width="160" height="192"/>
+</tileset>
