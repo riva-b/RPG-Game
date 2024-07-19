0bc2eef9a56754f66f539f5959933aea69084818
John Doe <johndoe@example.com>
2024-07-19 22:20:26 +0200

    ole

--- Vorlagen/Tiled Map/Kurve.tsx
+++ Vorlagen/Tiled Map/Kurve.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="Kurve" tilewidth="16" tileheight="16" tilecount="256" columns="16">
- <image source="../../../../../TEST/Kurve.png" trans="ffdcfa" width="256" height="256"/>
+ <image source="Kurve.png" trans="ffdcfa" width="256" height="256"/>
 </tileset>
